17077b5cf58f6b7365e669b25c7aa69764e042c4
Milton Orellana <Orellanamilton@hotmail.com>
2021-05-15 17:51:54 -0300

    Initial Commit

---
+++ NewsAPP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsAPP
+//
+//  Created by Milton Orellana on 15/05/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
